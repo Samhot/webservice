13080ceab06bd0b7187787eec30ea195af52c96d
samhot <thomas.punzano@gmail.com>
2016-10-21 02:01:37 +0200

    insertMatched ok

--- insertMatched.php
+++ insertMatched.php
@@ -1,17 +1,25 @@
-<?php $hostname = '164.132.195.67'; /*** mysql username ***/ $username = 'root'; /*** mysql password ***/ $password = 'abcd4ABCD'; function db_connect ($hostname, $username, 
-$password){
-    $bdd = new PDO("mysql:host=$hostname;dbname=mybookingtrainig", $username, $password);
-    return $bdd; try {
-    $bdd = db_connect ($hostname, $username, $password);
+<?php
+$servername = "164.132.195.67";
+$username = "root";
+$password = "toma66/moii";
+$dbname = "mybookingtraining";
     $idAdvert=$_GET["idAdvert"];
     $idCandidate=$_GET["idCandidate"];
-    $type=$_GET["type"];
-        $stmt = $bdd->prepare("INSERT INTO Macthed(`idAdvert`, `idCandidate`, `Type`) VALUES ($idAdvert, $idCandidate, $type)");
-        $nombre = $stmt->execute();
-        if($nombre == 1)
-        {
-        }
-    }
-} catch(PDOException $e) {
-    echo $e->getMessage();
+    $type=$_GET["Type"];
+
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+    // set the PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $sql = "INSERT INTO `mybookingtraining`.`Matched` (`idMatch`, `idAdvert`, `idCandidate`, `Type`) VALUES (NULL, $idAdvert, $idCandidate, $type)";
+    // use exec() because no results are returned
+    $conn->exec($sql);
+    echo "New record created successfully";
 }
+catch(PDOException $e)
+{
+    echo $sql . "<br>" . $e->getMessage();
+}
+
+$conn = null;
+?>
